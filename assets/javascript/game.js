9115f5223b4a20a71275f0726e1ea20baf3388b5
Andrea Carter <40808764+mirtacarter@users.noreply.github.com>
2018-07-22 18:38:10 -0400

    updated functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,77 +1,104 @@
-$(document).ready(function() {
-// Global variables
-//=========================================================
-    
-//Random number to be generated and displayed
-var targetNumber = 0;
+$(document).ready(function () {
+    // Global variables
+    //=========================================================
 
-// Array for possible crystal image values
-var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-// Generate random click value
-var clickValue = numberOptions[Math.floor(Math.random()*numberOptions.length)];
+    //Random number to be generated and displayed
+    var targetNumber = 0;
+    var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    var clickValue = 0;
 
-// Game counters - current score, total wins, and total losses
-var counter = 0;
-var wins = 0;
-var losses = 0;
+    // Game counters - current score, total wins, and total losses
+    var counter = 0;
+    var wins = 0;
+    var losses = 0;
 
-// Check to make sure game is not yet in play
-var isGameStarted = false;
+    // Check to make sure game is not yet in play
+    var isGameStarted = false;
 
-// Default content containers to empty
+    // Default content containers to empty
+    $("#number-played", "#current-score", "#gameResult", "#winCount", "#lossCount").empty();
 
-$("#number-played", "#current-score", "#gameResult", "#winCount", "#lossCount").empty();
+    // Functions
+    //=========================================================
 
-// Functions
-//=========================================================
+    // Start Game
+    function startGame() {
+        if (isGameStarted) {
+            // Alert user that game is in play
+            alert("Game is already in play! Click on a crystal to continue!");
+        }
+        else {
+            // Set boolean to true
+            isGameStarted = true;
+            // Reset counter
+            counter = 0;
+            // Generate random number
+            targetNumber = Math.floor(Math.random() * 120) + 19;
 
-// Start Game
-$(".start").on("click", function() {
-if (isGameStarted) {
-    // Alert user that game is in play
-    alert("Game is already in play! Click on a crystal to continue!");
-}
-else {
-    // Set boolean to true
-    isGameStarted = true;
-    // Reset counter
-    counter = 0;
-    // Generate random number
-    targetNumber = Math.floor(Math.random() * 120) + 19;
-
-    // Populate game content
-    console.log("Your number is: " + targetNumber);
-    console.log("Current score: " + counter);
-    console.log("Total wins: " + wins);
-    console.log("Total losses: " + losses);
+            // Populate game content
+            $("#number-played").text("Your number is: " + targetNumber);
+            $("#current-score").text("Current score: " + counter);
+            $("#winCount").text("Total wins: " + wins);
+            $("#lossCount").text("Total losses: " + losses);
+        }
     }
-});
 
-// Game logic
-$(".crystal-image").on("click", function() {
 
-    // Generate click value attribute called imgValue to crystal images using a For Loop
-    for (var i = 0; i < 5; i++) {
-    var crystalImg  = $(".crystal-image");
-    crystalImg.attr("imgValue", clickValue[i]);
-  };
+    // Game logic
+    function inPlay() {
+        if (isGameStarted) {
+            $(".crystal-image").click(function() {
 
-    // Apply attribute to images
-    var crystalValue = ($(this).attr("imgValue"));
-    // Parse integers so values can be added to score counter
-    crystalValue = parseInt(crystalValue);
-    // Add value to counter
-    counter += crystalValue;
+                // Generate random click value
+                for (var i = 0; i < numberOptions.length; i++) {
+                    clickValue = numberOptions[Math.floor(Math.random() * numberOptions.length)];
+                }
+                $("img").each(function() {
+                    $(this).attr("data-imgValue", clickValue[i])
+                
+            
+        
+        
+            // Parse integers so values can be added to score counter
+            var crystalValue = ($(this).attr("data-imgValue"));
+            crystalValue = parseInt(crystalValue);
+
+            // Add value to counter
+            counter += crystalValue;
+            });
+            });
+        }
+        
+        else {
+        // Alert user that start button must be pressed
+        alert("Click on the Start button to begin!");
+            }
+        }
+    
+
 
     // Win-loss logic - display appropriate image and message and increment win or loss by 1
-    if (counter === targetNumber) {
-        $("#gameResult").html("<img src=../images/crystalwin.jpg> <br> The power of crystals were with you! YOU WIN!");
-        wins++;
+    function roundComplete() {
+        if (counter === targetNumber) {
+            $("#gameResult").html("<img src=assets/images/crystalwin.jpg> <br> The power of crystals were with you! YOU WIN!");
+            wins++;
+            startGame();
+        }
+        else if (counter >= targetNumber) {
+            $("#gameResult").html("<img src=assets/images/crystallose.jpg> <br> Your crystal must be low on energy. Better luck next time!");
+            losses++;
+            startGame();
+        }
     }
-    else if (counter >= targetNumber) {
-        $("#gameResult").html("<img src=../images/crystallose.jpg> <br> Your crystal must be low on energy. Better luck next time!");
-        losses++;
-    }
-});
-});
 
+
+    // Main Game Process
+    //=========================================================
+
+    $(".start").on("click", function () {
+        startGame();
+     });    
+        inPlay();
+        roundComplete();
+       
+});
