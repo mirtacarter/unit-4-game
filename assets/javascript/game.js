8888d70901e0f8fdf72f30e1c6db44720df5000c
Andrea Carter <40808764+mirtacarter@users.noreply.github.com>
2018-07-22 19:51:52 -0400

    updated inplay function to add value on image click to current score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,9 @@
-$(document).ready(function () {
+$(document).ready(function() {
     // Global variables
     //=========================================================
 
     //Random number to be generated and displayed
     var targetNumber = 0;
-    var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    var clickValue = 0;
 
     // Game counters - current score, total wins, and total losses
     var counter = 0;
@@ -37,7 +35,6 @@ $(document).ready(function () {
 
             // Populate game content
             $("#number-played").text("Your number is: " + targetNumber);
-            $("#current-score").text("Current score: " + counter);
             $("#winCount").text("Total wins: " + wins);
             $("#lossCount").text("Total losses: " + losses);
         }
@@ -46,39 +43,15 @@ $(document).ready(function () {
 
     // Game logic
     function inPlay() {
-        if (isGameStarted) {
-            $(".crystal-image").click(function() {
-
-                // Generate random click value
-                for (var i = 0; i < numberOptions.length; i++) {
-                    clickValue = numberOptions[Math.floor(Math.random() * numberOptions.length)];
-                }
-                $("img").each(function() {
-                    $(this).attr("data-imgValue", clickValue[i])
-                
-            
         
+        $(".crystal-image").on("click", function() {
+        var clickValue = numberOptions[Math.floor(Math.random() * numberOptions.length)];
+            counter += clickValue;
+          $("#current-score").text("Current score: " + counter);
+         });
         
-            // Parse integers so values can be added to score counter
-            var crystalValue = ($(this).attr("data-imgValue"));
-            crystalValue = parseInt(crystalValue);
-
-            // Add value to counter
-            counter += crystalValue;
-            });
-            });
-        }
-        
-        else {
-        // Alert user that start button must be pressed
-        alert("Click on the Start button to begin!");
-            }
-        }
-    
-
-
     // Win-loss logic - display appropriate image and message and increment win or loss by 1
-    function roundComplete() {
+  
         if (counter === targetNumber) {
             $("#gameResult").html("<img src=assets/images/crystalwin.jpg> <br> The power of crystals were with you! YOU WIN!");
             wins++;
@@ -97,8 +70,11 @@ $(document).ready(function () {
 
     $(".start").on("click", function () {
         startGame();
-     });    
         inPlay();
-        roundComplete();
-       
+    
+      });      
+      
+      // Current score array
+    var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
 });
