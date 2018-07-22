59abc5672b7b7b5a933d738c79d00d6e5deff3c1
Andrea Carter <40808764+mirtacarter@users.noreply.github.com>
2018-07-22 15:15:36 -0400

    update to img divs and ids, started game code

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+// Create object with game result - a specific image and message will appear based on a win or loss
+var gameResult = {
+    youWon: {
+        picture: "../images/crystalwin.jpg",
+        message: "The power of crystals were with you! YOU WIN!"
+    },
+    youLost: {
+        picture: "../images/crystallose.jpg",
+        message: "Your crystal must be low on energy. Better luck next time!"
+    }
+};
+});
